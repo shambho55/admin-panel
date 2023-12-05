58c6ee25a79342b2fcb368d95700c2b5447ed244
shambho55 <rishabhchaturvedi579@gmail.com>
2023-12-06 02:01:56 +0530

    check

---
+++ helloworls/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Everyone</h1>
+      <ul>
+        <li>Hug Me</li>
+        <li>Love Me</li>
+        <li>Hate Me</li>
+      </ul>
+     <h3>But I Don't Care</h3>
+    </div>
+  );
+}
+
+export default App;
